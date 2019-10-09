432359db902d38fde79ac3438376d7fbf08036ac
Carleigh Crockett <capleugh@gmail.com>
2019-10-09 09:42:59 -0600

    Add rooms_from_category_method

--- lib/house.rb
+++ lib/house.rb
@@ -10,4 +10,11 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      # require "pry"; binding.pry
+      room.category == category
+    end
+  end
 end
